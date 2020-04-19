8769a6060b23479b218f4c9a3c9e058bfe775eb9
shraddha0602 <shraddha.tripathi6@gmail.com>
2020-04-19 20:04:23 +0530

    2nd commit

--- OrganChain/client_src/src/components/organTestedReport.js
+++ OrganChain/client_src/src/components/organTestedReport.js
@@ -4,67 +4,57 @@ import { Link } from 'react-router-dom';
 import {Navigation} from './NavHospital';
 import image2base64 from 'image-to-base64';
 
-class Patients extends Component{
+class organTestedReport extends Component{
 
-    state = { selectedFile: null ,
-        hashString : ""
-    }
+    state = { selectedFile: null }
 
     fileChangedHandler = event => {
         this.setState({ selectedFile: event.target.files[0] })
       }
 
-      fileToBase64 = (filename, filepath) => {
-        return new Promise(resolve => {
-          var file = new File([filename], filepath);
-          var reader = new FileReader();
-          // Read file content on file loaded event
-          reader.onload = function(event) {
-            resolve(event.target.result);
-          };
-          
-          // Convert data to base64 
-          reader.readAsDataURL(file);
-        });
-      };
-
     updateOrgan(updateOrgan) {
-        axios.request({
-            method:'post',
-            url:'http://localhost:3000/api/organTestedReportPrepared?access_token='+localStorage.ptspotter_accessToken,
-            data: updateOrgan
-          }).then(response => {
-            console.log("done");
-          }).catch(err => console.log(err));
+      axios.request({
+          method:'post',
+          url:'http://localhost:3000/api/organTestedReportPrepared?access_token='+localStorage.ptspotter_accessToken,
+          data: updateOrgan
+        }).then(response => {
+          console.log("done");
+        }).catch(err => console.log(err));
+  }
+
+    getOrgan(){
+      axios.get('http://localhost:3000/api/Organ?Organ_findById='+this.refs.organ_id.value)
+        .then(response => {
+          response.Tested.value=true;
+          response.testTime.value=new Date().now();
+          this.updateOrgantime(response);
+      })
+      .catch(err => console.log(err));
     }
 
+    updateOrgantime(organDetails) {
+      axios.request({
+          method:'put',
+          url:'http://localhost:3000/api/Organ?Organ_replaceById='+this.refs.organ_id.value,
+          data: organDetails
+        }).then(response => {
+          console.log("done");
+        }).catch(err => console.log(err));
+  }
+
     onSubmit(e){
+        const updateOrgan = {
+            "$class": "org.organ.net.organTestedReportPrepared",
+            hash: btoa(this.state.selectedFile),
+            organ: "resource:org.organ.net.Organ#"+this.refs.organ_id.value,
+            donor: "resource:org.organ.net.Donor#"+this.refs.donor_id.value
+        }
 
-        //var hashString = btoa(this.state.selectedFile);
-        this.fileToBase64("download.jpeg", "./test.pdf").then((result) => {
-            console.log("test : "+result);
-          });
-        image2base64(this.state.selectedFile).then(
-            (response) => {
-
-                const updateOrgan = {
-                    "$class": "org.organ.net.organTestedReportPrepared",
-                    hash: response,
-                    organ: "resource:org.organ.net.Organ#"+this.refs.organ_id.value,
-                    donor: "resource:org.organ.net.Donor#"+this.refs.donor_id.value
-                }
-                    console.log(atob(updateOrgan.hash));
-                this.updateOrgan(updateOrgan);   
-            }
-        ).catch(
-            (error) => {
-                console.log(error);
-            }
-        )   
-            this.refs.donor_id.value="";
-            this.refs.organ_id.value="";
-            this.state.selectedFile="";
-            e.preventDefault();
+        this.updateOrgan(updateOrgan);  
+       // this.getOrgan();     
+        this.refs.donor_id.value="";
+        this.refs.organ_id.value="";
+        e.preventDefault();
     }
 
     render() {
@@ -99,4 +89,4 @@ class Patients extends Component{
     }
 }
 
-export default Patients;
+export default organTestedReport;
